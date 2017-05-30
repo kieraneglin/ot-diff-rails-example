b1aefb145c49aba8ea78e155e5df305fe32ed162
Kieran Eglin <kieran.eglin@gmail.com>
2017-05-29 20:48:40 -0700

    updated op-diff and refactored app

--- app/javascript/packs/diff_helper.js
+++ app/javascript/packs/diff_helper.js
@@ -1,37 +1,42 @@
 import OtDiff from 'ot-diff';
+import unirest from 'unirest';
 
 class DiffHelper {
   connected(opts, App) {
-    let postId = document.getElementById('post_id').value;
-
     opts.textarea = document.getElementById('post_body');
     opts.content = opts.textarea.value;
     opts.identifier = Math.random().toString();
 
     opts.textarea.addEventListener('input', () => {
-      opts.transform = OtDiff.diff(opts.content, opts.textarea.value);
-      opts.transform.sender = opts.identifier;
-
-      App.sendTransform(postId, opts.transform);
-      opts.content = opts.textarea.value;
+      this._applyDiff(opts);
     });
 
     return opts;
   }
-  received(response, opts, App) {
+  received(response, opts) {
     let transform = JSON.parse(response.transform);
 
     if(transform.sender !== opts.identifier) {
-      if(transform.action === 'insert') {
-        App.insertTransform(opts, transform);
-      } else if(transform.action === 'delete') {
-        App.deleteTransform(opts, transform);
-      } else if(transform.action === 'replace') {
-        App.replaceTransform(opts, transform);
-      } else if(transform.action === 'noop') {
-        return;
-      }
+      let updated = OtDiff.transform(opts.textarea.value, transform);
+
+      opts.content = updated;
+      opts.textarea.value = updated;
     }
+  }
+  _applyDiff(opts) {
+    let postId = document.getElementById('post_id').value;
+
+    opts.transform = OtDiff.diff(opts.content, opts.textarea.value);
+    opts.transform.sender = opts.identifier;
+
+    this._sendTransform(postId, opts.transform);
+    opts.content = opts.textarea.value;
+  }
+  _sendTransform(post, transform) {
+    let url = `http://localhost:3000/transforms/${post}`;
+    let body = { transform: JSON.stringify(transform) };
+
+    unirest.patch(url).send(body).end();
   }
 }
 
--- app/javascript/packs/ot.js
+++ app/javascript/packs/ot.js
@@ -4,62 +4,12 @@ import App from './posts_cable';
 import DiffHelper from './diff_helper';
 
 let opts = {};
+
 App.posts = App.cable.subscriptions.create('PostsChannel', {
   connected: () => {
-    DiffHelper.connected(opts, App);
+    DiffHelper.connected(opts);
   },
   received: (data) => {
-    DiffHelper.received(data, opts, App);
+    DiffHelper.received(data, opts);
   }
 });
-
-App.sendTransform = (post, transform) => {
-  let url = `http://localhost:3000/transforms/${post}`;
-  let body = { transform: JSON.stringify(transform) };
-
-  unirest.patch(url).send(body).end();
-};
-
-App.insertTransform = (opts, transform) => {
-  let string = opts.textarea.value;
-  let updated = string.slice(0, transform.start) + transform.payload + string.slice(transform.start);
-
-  opts.content = updated;
-  opts.textarea.value = updated;
-};
-
-App.deleteTransform = (opts, transform) => {
-  let string = opts.textarea.value;
-  let updated = string.slice(0, transform.start) + string.slice(transform.start + transform.remove);
-
-  opts.content = updated;
-  opts.textarea.value = updated;
-};
-
-App.replaceTransform = (opts, transform) => {
-  let string = opts.textarea.value;
-  let removed = string.slice(0, transform.start) + string.slice(transform.start + transform.remove);
-  let updated = removed.slice(0, transform.start) + transform.payload + removed.slice(transform.start);
-
-  opts.content = updated;
-  opts.textarea.value = updated;
-};
-
-
-
-
-// document.addEventListener('turbolinks:load', () => {
-//   let textarea = document.getElementById('post_body');
-//   let postId = document.getElementById('post_id').value;
-//
-//   window.consumerId = Math.random().toString();
-//   window.contents = textarea.value;
-//
-//   textarea.addEventListener('input', (e) => {
-//     let transform = OtDiff.diff(window.contents, textarea.value);
-//     transform.sender = window.consumerId;
-//
-//     PostsCable.sendTransform(postId, transform);
-//     window.contents = textarea.value;
-//   });
-// });
