dc15b58a6cc39fc805c7d7f2b866316bfed3bca1
Kieran Eglin <kieran.eglin@gmail.com>
2017-05-29 19:14:51 -0700

    set up basic commands for delete and replace

--- app/javascript/packs/ot.js
+++ app/javascript/packs/ot.js
@@ -1,19 +1,90 @@
 import OtDiff from 'ot-diff';
+import unirest from 'unirest';
+import App from './posts_cable';
 
-import PostsCable from './posts_cable';
+let opts = {};
+App.posts = App.cable.subscriptions.create('PostsChannel', {
+  connected: () => {
+    let postId = document.getElementById('post_id').value;
 
-document.addEventListener('turbolinks:load', () => {
-  let textarea = document.getElementById('post_body');
-  let postId = document.getElementById('post_id').value;
+    opts.textarea = document.getElementById('post_body');
+    opts.content = opts.textarea.value;
+    opts.identifier = Math.random().toString();
 
-  window.consumerId = Math.random().toString();
-  window.contents = textarea.value;
+    opts.textarea.addEventListener('input', () => {
+      opts.transform = OtDiff.diff(opts.content, opts.textarea.value);
+      opts.transform.sender = opts.identifier;
 
-  textarea.addEventListener('input', (e) => {
-    let transform = OtDiff.diff(window.contents, textarea.value);
-    transform.sender = window.consumerId;
+      App.sendTransform(postId, opts.transform);
+      opts.content = opts.textarea.value;
+    });
+  },
+  received: (data) => {
+    let transform = JSON.parse(data.transform);
 
-    PostsCable.sendTransform(postId, transform);
-    window.contents = textarea.value;
-  });
+    if(transform.sender !== opts.identifier) {
+      if(transform.action === 'insert') {
+        App.insertTransform(opts, transform);
+      } else if(transform.action === 'delete') {
+        App.deleteTransform(opts, transform);
+      } else if(transform.action === 'replace') {
+        App.replaceTransform(opts, transform);
+      } else if(transform.action === 'noop') {
+        return;
+      }
+    } else {
+      console.log('from me');
+    }
+  }
 });
+
+App.sendTransform = (post, transform) => {
+  let url = `http://localhost:3000/transforms/${post}`;
+  let body = { transform: JSON.stringify(transform) };
+
+  unirest.patch(url).send(body).end();
+};
+
+App.insertTransform = (opts, transform) => {
+  let string = opts.textarea.value;
+  let updated = string.slice(0, transform.start) + transform.payload + string.slice(transform.start);
+
+  opts.content = updated;
+  opts.textarea.value = updated;
+};
+
+App.deleteTransform = (opts, transform) => {
+  let string = opts.textarea.value;
+  let updated = string.slice(0, transform.start) + string.slice(transform.start + transform.remove);
+
+  opts.content = updated;
+  opts.textarea.value = updated;
+};
+
+App.replaceTransform = (opts, transform) => {
+  let string = opts.textarea.value;
+  let removed = string.slice(0, transform.start) + string.slice(transform.start + transform.remove);
+  let updated = removed.slice(0, transform.start) + transform.payload + removed.slice(transform.start);
+
+  opts.content = updated;
+  opts.textarea.value = updated;
+};
+
+
+
+
+// document.addEventListener('turbolinks:load', () => {
+//   let textarea = document.getElementById('post_body');
+//   let postId = document.getElementById('post_id').value;
+//
+//   window.consumerId = Math.random().toString();
+//   window.contents = textarea.value;
+//
+//   textarea.addEventListener('input', (e) => {
+//     let transform = OtDiff.diff(window.contents, textarea.value);
+//     transform.sender = window.consumerId;
+//
+//     PostsCable.sendTransform(postId, transform);
+//     window.contents = textarea.value;
+//   });
+// });
--- app/javascript/packs/posts_cable.js
+++ app/javascript/packs/posts_cable.js
@@ -1,40 +1,4 @@
-import unirest from 'unirest';
-
 this.App = {};
-
 App.cable = ActionCable.createConsumer();
-
-App.posts = App.cable.subscriptions.create('PostsChannel', {
-  connected: () => {
-    console.log('connected');
-  },
-  received: (data) => {
-    let transform = JSON.parse(data.transform);
-    let textarea = document.getElementById('post_body');
-
-    if(transform.action === 'insert') {
-      if(transform.sender === window.consumerId) {
-        console.log('from me');
-      } else {
-        let doc = textarea.value;
-        let updated = doc.slice(0, transform.start) + transform.payload + doc.slice(transform.start);
-        window.contents = updated;
-        textarea.value = updated;
-
-        // action:"insert"
-        // payload:"a"
-        // sender:"0.05636868005365292"
-        // start: 16
-      }
-    }
-  }
-});
-
-App.sendTransform = (post, transform) => {
-  let url = `http://localhost:3000/transforms/${post}`;
-  let body = { transform: JSON.stringify(transform) };
-
-  unirest.patch(url).send(body).end();
-};
 
 export default App;
