0eafb23da25a55880ead685012371511760aba83
Kieran Eglin <kieran.eglin@gmail.com>
2017-05-31 12:40:22 -0700

    server no longer saves each message

--- app/controllers/transforms_controller.rb
+++ app/controllers/transforms_controller.rb
@@ -2,8 +2,7 @@ class TransformsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def update
-    Post.find(params[:id]).update(body: params[:post])
-
+    # Post.find(params[:id]).update(body: params[:post])
     ActionCable.server.broadcast("posts-#{params[:id]}", transform: params[:transform])
 
     render json: {}
